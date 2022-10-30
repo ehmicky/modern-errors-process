4b3c0e306c4b02907f75200ed4f4ce1588c340d3
ehmicky <ehmicky@gmail.com>
2022-10-30 17:50:06 +0100

    Initial commit

--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -1,8 +1,36 @@
-import { expectType, expectAssignable } from 'tsd'
+import {
+  expectType,
+  expectAssignable,
+  expectNotAssignable,
+  expectError,
+} from 'tsd'
 
-import templateName, { Options } from './main.js'
+import modernErrors from '../main.js'
+import plugin, { Options, Event } from './process.js'
 
-expectType<object>(templateName(true))
+const AnyError = modernErrors([plugin])
+const undo = AnyError.logProcess()
 
-templateName(true, {})
+modernErrors([plugin], { process: {} })
+AnyError.logProcess({})
 expectAssignable<Options>({})
+modernErrors([plugin], { process: { exit: true } })
+AnyError.logProcess({ exit: true })
+expectAssignable<Options>({ exit: true })
+expectError(AnyError.logProcess(undefined))
+expectNotAssignable<Options>(undefined)
+expectError(modernErrors([plugin], { process: true }))
+expectError(AnyError.logProcess(true))
+expectNotAssignable<Options>(true)
+expectError(modernErrors([plugin], { process: { exit: 'true' } }))
+expectError(AnyError.logProcess({ exit: 'true' }))
+expectNotAssignable<Options>({ exit: 'true' })
+expectError(modernErrors([plugin], { process: { unknown: true } }))
+expectError(AnyError.logProcess({ unknown: true }))
+expectNotAssignable<Options>({ unknown: true })
+
+expectAssignable<Event>('rejectionHandled')
+expectNotAssignable<Event>('')
+
+expectType<void>(undo())
+expectError(undo(undefined))
