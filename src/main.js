b52aa708fc29b7831b2c3c221c4749c2384d6c76
ehmicky <ehmicky@gmail.com>
2022-11-13 16:01:19 +0100

    Fix code for modern-errors@v5

--- src/main.js
+++ src/main.js
@@ -1,33 +1,57 @@
+import isPlainObj from 'is-plain-obj'
 import logProcessErrors, { validateOptions } from 'log-process-errors'
 
-const getOptions = function (options) {
-  validateOptions(options)
-  return options
+const getOptions = function (options = {}) {
+  if (!isPlainObj(options)) {
+    throw new TypeError(`Options must be a plain object: ${options}`)
+  }
+
+  const { UnknownError, ...logProcessErrorsOptions } = options
+
+  if (UnknownError !== undefined && typeof UnknownError !== 'function') {
+    throw new TypeError(
+      `"UnknownError" option must be an error class: ${UnknownError}`,
+    )
+  }
+
+  validateOptions(logProcessErrorsOptions)
+  return { ...logProcessErrorsOptions, UnknownError }
 }
 
 // Forwards to `log-process-errors`
 const logProcess = function ({
-  options = {},
-  BaseError,
-  ErrorClasses: { UnknownError },
+  options: { onError, UnknownError, ...logProcessErrorsOptions },
+  ErrorClass,
 }) {
-  const onError = customOnError.bind(undefined, {
-    onError: options.onError,
-    BaseError,
+  if (UnknownError !== undefined && !isSubclass(UnknownError, ErrorClass)) {
+    throw new TypeError(
+      `"UnknownError" option "${UnknownError.name}" must be a subclass of ${ErrorClass.name}`,
+    )
+  }
+
+  const onErrorA = customOnError.bind(undefined, {
+    onError,
+    ErrorClass,
     UnknownError,
   })
-  return logProcessErrors({ ...options, onError })
+  return logProcessErrors({ ...logProcessErrorsOptions, onError: onErrorA })
 }
+
+const isSubclass = function (ErrorClass, ParentClass) {
+  return ParentClass === ErrorClass || isProtoOf.call(ParentClass, ErrorClass)
+}
+
+const { isPrototypeOf: isProtoOf } = Object.prototype
 
 // Process errors always indicate unknown behavior. Therefore, we wrap them
 // as `UnknownError` even if the underlying class is known.
 // This applies whether `onError` is overridden or not.
 const customOnError = async function (
-  { onError = defaultOnError, BaseError, UnknownError },
+  { onError = defaultOnError, ErrorClass, UnknownError },
   error,
   ...args
 ) {
-  const unknownError = normalizeError(error, BaseError, UnknownError)
+  const unknownError = normalizeError(error, ErrorClass, UnknownError)
   await onError(unknownError, ...args)
 }
 
@@ -37,9 +61,11 @@ const defaultOnError = function (error) {
   console.error(error)
 }
 
-const normalizeError = function (error, BaseError, UnknownError) {
-  const cause = BaseError.normalize(error)
-  return cause instanceof UnknownError ? cause : new UnknownError('', { cause })
+const normalizeError = function (error, ErrorClass, UnknownError) {
+  const cause = ErrorClass.normalize(error)
+  return UnknownError === undefined || cause instanceof UnknownError
+    ? cause
+    : new UnknownError('', { cause })
 }
 
 export default {
--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@ import { emitWarning } from 'node:process'
 import { promisify } from 'node:util'
 
 import test from 'ava'
-import modernErrors from 'modern-errors'
+import ModernError from 'modern-errors'
 import modernErrorsProcess from 'modern-errors-process'
 import sinon from 'sinon'
 import { each } from 'test-each'
@@ -10,13 +10,23 @@ import { each } from 'test-each'
 // TODO: use `timers/promises` after dropping support for Node <15
 const pSetInterval = promisify(setInterval)
 
-const BaseError = modernErrors([modernErrorsProcess])
+const BaseError = ModernError.subclass('BaseError', {
+  plugins: [modernErrorsProcess],
+})
 const UnknownError = BaseError.subclass('UnknownError')
 const ChildUnknownError = UnknownError.subclass('ChildUnknownError')
 const TestError = BaseError.subclass('TestError')
 
 each(
-  [true, { unknown: true }, { exit: 'true' }, { onError: true }],
+  [
+    true,
+    { unknown: true },
+    { exit: 'true' },
+    { onError: true },
+    { UnknownError: true },
+    { UnknownError: Error },
+    { UnknownError: ModernError },
+  ],
   ({ title }, options) => {
     test.serial(`Options are validated | ${title}`, (t) => {
       t.throws(BaseError.logProcess.bind(BaseError, options))
@@ -33,7 +43,7 @@ test.serial('Prints on the console by default', async (t) => {
   // eslint-disable-next-line no-restricted-globals
   const consoleError = sinon.stub(console, 'error')
   const stopLogging = BaseError.logProcess()
-  const error = new UnknownError('test')
+  const error = new BaseError('test')
   await createProcessError(error)
   t.is(consoleError.args[0][0], error)
   stopLogging()
@@ -43,7 +53,7 @@ test.serial('Prints on the console by default', async (t) => {
 test.serial('Handles process errors', async (t) => {
   const onError = sinon.spy()
   const stopLogging = BaseError.logProcess({ onError })
-  const error = new UnknownError('test')
+  const error = new BaseError('test')
   await createProcessError(error)
   t.deepEqual(onError.args, [[error, 'warning']])
   stopLogging()
@@ -53,6 +63,12 @@ each(
   [
     { error: 'test', message: 'Warning: test' },
     { error: new Error('test'), message: 'test' },
+    {
+      error: new Error('test'),
+      message: 'test',
+      passUnknown: false,
+      expectedConstructor: BaseError,
+    },
     { error: new TypeError('test'), message: 'test' },
     {
       // eslint-disable-next-line fp/no-mutating-assign
@@ -60,17 +76,41 @@ each(
       message: 'NamedError: test',
     },
     { error: new UnknownError('test'), message: 'test' },
-    { error: new ChildUnknownError('test'), message: 'test' },
+    { error: new UnknownError('test'), message: 'test', passUnknown: false },
+    {
+      error: new ChildUnknownError('test'),
+      message: 'test',
+      expectedConstructor: ChildUnknownError,
+    },
+    {
+      error: new ChildUnknownError('test'),
+      message: 'test',
+      passUnknown: false,
+      expectedConstructor: ChildUnknownError,
+    },
     { error: new TestError('test'), message: 'TestError: test' },
+    {
+      error: new TestError('test'),
+      message: 'test',
+      passUnknown: false,
+      expectedConstructor: TestError,
+    },
   ],
-  ({ title }, { error, message }) => {
+  (
+    { title },
+    { error, message, passUnknown = true, expectedConstructor = UnknownError },
+  ) => {
     test.serial(
       `Process errors are normalized to UnknownError | ${title}`,
       async (t) => {
         const onError = sinon.spy()
-        const stopLogging = BaseError.logProcess({ onError })
+        const UnknownErrorOpt = passUnknown ? UnknownError : undefined
+        const stopLogging = BaseError.logProcess({
+          onError,
+          UnknownError: UnknownErrorOpt,
+        })
         await createProcessError(error)
-        t.true(onError.args[0][0] instanceof UnknownError)
+        t.is(onError.args[0][0].constructor, expectedConstructor)
         t.is(onError.args[0][0].message, message)
         stopLogging()
       },
