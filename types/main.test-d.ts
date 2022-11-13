235c2b5b87371ae2011df21c6e08b6e276d5d955
ehmicky <ehmicky@gmail.com>
2022-11-13 15:51:54 +0100

    Fix types

--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -5,27 +5,50 @@ import {
   expectError,
 } from 'tsd'
 
-import modernErrors from 'modern-errors'
+import ModernError from 'modern-errors'
 import modernErrorsProcess, { Options, Event } from 'modern-errors-process'
 
-const BaseError = modernErrors([modernErrorsProcess])
+const BaseError = ModernError.subclass('BaseError', {
+  plugins: [modernErrorsProcess],
+})
 const undo = BaseError.logProcess()
 
-modernErrors([modernErrorsProcess], { process: {} })
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsProcess],
+  process: {},
+})
 BaseError.logProcess({})
 expectAssignable<Options>({})
-modernErrors([modernErrorsProcess], { process: { exit: true } })
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsProcess],
+  process: { exit: true },
+})
 BaseError.logProcess({ exit: true })
 expectAssignable<Options>({ exit: true })
 expectError(BaseError.logProcess(undefined))
 expectNotAssignable<Options>(undefined)
-expectError(modernErrors([modernErrorsProcess], { process: true }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsProcess],
+    process: true,
+  }),
+)
 expectError(BaseError.logProcess(true))
 expectNotAssignable<Options>(true)
-expectError(modernErrors([modernErrorsProcess], { process: { exit: 'true' } }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsProcess],
+    process: { exit: 'true' },
+  }),
+)
 expectError(BaseError.logProcess({ exit: 'true' }))
 expectNotAssignable<Options>({ exit: 'true' })
-expectError(modernErrors([modernErrorsProcess], { process: { unknown: true } }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsProcess],
+    process: { unknown: true },
+  }),
+)
 expectError(BaseError.logProcess({ unknown: true }))
 expectNotAssignable<Options>({ unknown: true })
 
